10948b9ada992a16ffcfe6d51bd319ad3e6feb49
jinwoo park <swat018@gmail.com>
2022-09-10 23:05:16 +0900

    feat : 싱글톤 패턴 2부 - 이른 초기화 (eager initialization)을 사용하는 방법

--- src/main/java/io/swat018/designpatterns/creational_patterns/singleton/Settings.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/singleton/Settings.java
@@ -2,15 +2,11 @@ package io.swat018.designpatterns.creational_patterns.singleton;
 
 public class Settings {
 
-    private static Settings instance;
+    private static final Settings INSTANCE = new Settings();
     private Settings() {}
 
-    public static synchronized Settings getInstance() {
-        if (instance == null) {
-            instance = new Settings();
-        }
-
-        return instance;
+    public static Settings getInstance() {
+        return INSTANCE                                                                         ;
     }
 
 }
