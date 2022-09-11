10bc5a1fc8d438739b8ad3c28a4395de32c24752
jinwoo park <swat018@gmail.com>
2022-09-11 17:37:26 +0900

    feat : 싱글톤 패턴 3부 - 싱글톤 패턴 구현 깨트리는 방법 1(리플렉션)

--- src/main/java/io/swat018/designpatterns/creational_patterns/singleton/App.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/singleton/App.java
@@ -1,10 +1,17 @@
 package io.swat018.designpatterns.creational_patterns.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
-        Settings settings1 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        constructor.newInstance()
+
         System.out.println(settings == settings1);
     }
 }
