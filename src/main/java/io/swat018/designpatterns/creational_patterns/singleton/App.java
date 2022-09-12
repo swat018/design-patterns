08ff657b7a72e1d3b300400320aa78fa60d094af
jinwoo park <swat018@gmail.com>
2022-09-12 12:52:50 +0900

    feat : 싱글톤 패턴 4부 - 안전하고 단순하게 구현하는 방법(enum을 사용하는 방법)

--- src/main/java/io/swat018/designpatterns/creational_patterns/singleton/App.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/singleton/App.java
@@ -7,15 +7,15 @@ import java.lang.reflect.InvocationTargetException;
 public class App {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings settings = Settings.getInstance();
-        Settings settings1 = null;
+        Settings settings = Settings.INSTANCE;
 
+        Settings settings1 = null;
         try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
             out.writeObject(settings);
         }
 
-        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings1 = (Settings) in.readObject();
+        try (ObjectInput input = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings1 = (Settings) input.readObject();
         }
 
         System.out.println(settings == settings1);
--- src/main/java/io/swat018/designpatterns/creational_patterns/singleton/Settings.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/singleton/Settings.java
@@ -2,20 +2,8 @@ package io.swat018.designpatterns.creational_patterns.singleton;
 
 import java.io.Serializable;
 
-public class Settings implements Serializable {
+public enum Settings {
 
-    private Settings() {}
-
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    public static Settings getInstance() {
-        return SettingsHolder.INSTANCE;
-    }
-
-    protected Object readResolve() {
-        return getInstance();
-    }
+    INSTANCE;
 
 }
