fdaa1eade61c036127e513dad0905582ae557c75
jinwoo park <swat018@gmail.com>
2022-09-25 00:03:02 +0900

    feat : 추상 팩토리 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

--- src/main/java/io/swat018/designpatterns/creational_patterns/abstract_factory/java/FactoryBeanConfig.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/abstract_factory/java/FactoryBeanConfig.java
@@ -1,4 +1,13 @@
 package io.swat018.designpatterns.creational_patterns.abstract_factory.java;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FactoryBeanConfig {
+
+    @Bean
+    public ShipFactory shipFactory() {
+        return new ShipFactory();
+    }
 }
--- src/main/java/io/swat018/designpatterns/creational_patterns/abstract_factory/java/FactoryBeanExample.java
+++ src/main/java/io/swat018/designpatterns/creational_patterns/abstract_factory/java/FactoryBeanExample.java
@@ -2,13 +2,19 @@ package io.swat018.designpatterns.creational_patterns.abstract_factory.java;
 
 import io.swat018.designpatterns.creational_patterns.factorymethod.after.Ship;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class FactoryBeanExample {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-        Ship whiteship = applicationContext.getBean("whiteship", Ship.class);
-        System.out.println(whiteship.getName());
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+//        Ship whiteship = applicationContext.getBean("whiteship", Ship.class);
+//        System.out.println(whiteship.getName());
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(FactoryBeanConfig.class);
+//        ShipFactory bean = applicationContext.getBean(ShipFactory.class);
+        Ship bean = applicationContext.getBean(Ship.class);
+        System.out.println(bean);
     }
 }
